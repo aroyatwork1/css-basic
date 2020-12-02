318e3607ec032b05497c81bad9f269db4ab32cf3
Atanu Roy <atanuroy@Atanus-MacBook-Air.local>
2020-12-02 20:43:40 +0530

    Committing first vue application

---
+++ basic-vue/main.js
@@ -0,0 +1,14 @@
+Vue.createApp({
+	data() {
+		return {
+			goals: [],
+			enteredGoal: ''
+		};
+	},
+	methods: {
+		addGoal() {
+			this.goals.push(this.enteredGoal);
+			this.enteredGoal = '';
+		}
+	}
+}).mount("#app");
